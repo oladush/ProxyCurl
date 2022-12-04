ccd0dc5005dd3be35abe8edb1171c29b8b1acf5e
olad <61180708+oladush@users.noreply.github.com>
2022-12-04 14:34:31 +0000

    litle fix & proxy list update

--- ProxyCurl.sh
+++ ProxyCurl.sh
@@ -9,6 +9,7 @@ echo -e "\e[32mSending GET request using"
 echo -e "\e[37m "
 echo "Proxy number $n : $line"
 echo -e "\e[92m "
+line="${line%%[[:cntrl:]]}"
 curl -x $line -m 2.37 -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1" $2
 n=$((n+1))
 done < $filename
